6c9b40fb93a982dcda87daa8afd005a430778f96
Kristóf Balli <kristof.balli@stud.uni-corvinus.hu>
2020-09-22 14:42:52 +0200

    Külső fájlba mentés

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using UserMaintenance.Entities;
+using System.IO;
 
 namespace UserMaintenance
 {
@@ -20,7 +21,7 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             
             button1.Text = Resource1.Add;
-
+            button2.Text = Resource1.Write;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
@@ -34,6 +35,30 @@ namespace UserMaintenance
                 
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (FileStream file = new FileStream(sfd.FileName, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    foreach (User user in users)
+                    {
+                        writer.WriteLine("{0};{1}",
+                            user.ID, user.FullName);
+                    }
+                }
+            }
         }
     }
 }
