82122a4336b15c6ab67a53443499edc2c8cccd9a
Kristóf Balli <kristof.balli@stud.uni-corvinus.hu>
2020-10-01 14:27:07 +0200

    Adatok betöltése

--- gyak4/Form1.cs
+++ gyak4/Form1.cs
@@ -12,9 +12,17 @@ namespace gyak4
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
