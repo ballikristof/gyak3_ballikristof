d1e49e86a3ca5bd444919b493126cdd0be3c7eb8
Kristóf Balli <kristof.balli@stud.uni-corvinus.hu>
2020-10-07 18:21:01 +0200

    Portfólió tárolása a memóriában

--- gyak5_2/gyak5_2/Form1.cs
+++ gyak5_2/gyak5_2/Form1.cs
@@ -14,11 +14,21 @@ namespace gyak5_2
     {
         List<Tick> Ticks;
         PortfolioEntities context = new PortfolioEntities();
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
