c205530fd027c549583c3031ca6449541dace80a
Kristóf Balli <kristof.balli@stud.uni-corvinus.hu>
2020-10-17 16:40:50 +0200

    Webszolgáltatás hívása

--- gyak6/gyak6/Form1.cs
+++ gyak6/gyak6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak6.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,24 @@ namespace gyak6
         public Form1()
         {
             InitializeComponent();
+            ServiceHivas();
+            
+        }
+
+        public void ServiceHivas()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate= "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
