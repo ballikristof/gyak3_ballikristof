88e4773c53bebe47879fde9bbe134aff469abf19
Kristóf Balli <kristof.balli@stud.uni-corvinus.hu>
2020-10-17 17:36:36 +0200

    XML feldolgozása

--- gyak6/gyak6/Form1.cs
+++ gyak6/gyak6/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using gyak6.Entities;
+using System.Xml;
 
 namespace gyak6
 {
@@ -20,6 +21,7 @@ namespace gyak6
             InitializeComponent();
             ServiceHivas();
             dataGridView1.DataSource = Rates;
+            
         }
 
         public void ServiceHivas()
@@ -36,6 +38,27 @@ namespace gyak6
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
+
+        
     }
 }
