df9fce84129b35f4037c3020e72db17a9a936411
Kristóf Balli <kristof.balli@stud.uni-corvinus.hu>
2020-10-25 13:51:17 +0100

    Osztályok létrehozása

---
+++ gyak7_2/gyak7_2/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak7_2.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NumOfChildren { get; set; }
+        public double BirthP { get; set; }
+    }
+}
