31ea9c8e7e9352b5f8e3b48cfa942da41b2c9eed
Kristóf Balli <kristof.balli@stud.uni-corvinus.hu>
2020-11-14 16:54:09 +0100

    Absztrakció felhasználása a futószalagban

--- gyak8/gyak8/Form1.cs
+++ gyak8/gyak8/Form1.cs
@@ -8,15 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using gyak8.Entities;
+using gyak8.Abstractions;
 
 namespace gyak8
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
+        private List<Toy> _toys = new List<Toy>();
 
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -29,8 +30,8 @@ namespace gyak8
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreteNew();
-            _balls.Add(ball);
+            var ball = Factory.CreateNew();
+            _toys.Add(ball);
             ball.Left = -ball.Width;
             mainpanel.Controls.Add(ball);
         }
@@ -38,7 +39,7 @@ namespace gyak8
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
                 ball.MoveToy();
                 if (ball.Left > maxPosition)
@@ -47,9 +48,9 @@ namespace gyak8
 
             if(maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainpanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
         }
     }
